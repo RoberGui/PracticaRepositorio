a2084ef029eae3deab950aecb8603e9b0bf515df
RoberGui <maritooguillen22@gmail.com>
2025-05-23 07:53:39 -0600

    Add files via upload
    commit para agregar java code

---
+++ TImeToPractice/src/main/java/package/TImeToPractice.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package package;
+
+/**
+ *
+ * @author marit
+ */
+public class TImeToPractice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ TImeToPractice/src/main/java/timetopractice/TImeToPractice_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package timetopractice;
+
+/**
+ *
+ * @author marit
+ */
+public class TImeToPractice_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
